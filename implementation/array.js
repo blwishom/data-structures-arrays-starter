1b4afcbde3df02b06322bc67c6cb75acae9cd8ac
Derek Jou <derekjou0@gmail.com>
2021-01-23 18:33:20 -0800

    initial commit, need npm init and test specs to be setup

---
+++ implementation/array.js
@@ -0,0 +1,32 @@
+class MyArray {
+    constructor(val) {
+      this.storage = new Array(val);
+      this.length = val;
+    }
+    //O(1) unless we've filled the array, then O(n) operation to add to the back of the array
+    push(val) {
+      
+
+    }
+    //O(1) operation to remove a value from the back of the array
+    pop() {
+   
+
+    }
+    //O(n) operation to remove a value from the front of the array
+    shift() {
+
+
+    }
+    //O(n) operation to add a value to the front of the array
+    unshift(val) {
+
+
+    }
+    //O(n) operation to find the index of a value, returns -1 
+    search(val) {
+
+
+    }
+  }
+  
---
+++ practice/arrayPractice.js
@@ -0,0 +1,47 @@
+const findMinimum = arr => {
+
+};
+
+const runningSum = arr => {
+
+};
+
+const evenNumOfChars = arr => {
+
+};
+
+const smallerThanCurr = arr => {
+
+};
+
+const twoSum = (arr, target) => {
+
+};
+
+const secondLargest = arr => {
+
+};
+
+const maxSumOfSubarrayOfLength = (arr, k) => {
+
+};
+
+const smallestSubarrayOfSum = (arr, s) => {
+
+};
+
+const shuffle = (arr, n) => {
+
+};
+
+const largestDistribution = (arr, n) => {
+
+};
+
+const validParentheses = str => {
+
+};
+
+const rpnCalculator = inputs => {
+
+};
